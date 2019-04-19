65f5eac500d7ed315ef4a8973f506f2d606fb1e7
abdelrahman <abdelrahman.o.a@ieee.org>
2019-04-19 19:37:07 +0200

    fix:add bottomsheet

--- app/src/main/java/com/chipin/chipin/MapActivity.java
+++ app/src/main/java/com/chipin/chipin/MapActivity.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.andrefrsousa.superbottomsheet.SuperBottomSheetFragment;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -21,7 +22,16 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.util.Objects;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MapActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.bottom_sheet)
+    LinearLayout layoutBottomSheet;
+    BottomSheetBehavior sheetBehavior;
+    String TAG = "bottomSheet";
 
 
     @Override
@@ -30,7 +40,41 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         MapView MapsFragment = new MapView();
+        layoutBottomSheet = findViewById(R.id.bottom_sheet);
         getSupportFragmentManager().beginTransaction().replace(R.id.map_container, MapsFragment, "").commit();
+
+        ButterKnife.bind(this);
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        Log.d(TAG, "onStateChanged: Close Sheet");
+//                        btnBottomSheet.setText("Close Sheet");
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        Log.d(TAG, "onStateChanged: Expand Sheet");
+//                        btnBottomSheet.setText("Expand Sheet");
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
 
 
     }
@@ -40,7 +84,6 @@ public class MapActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         finish();
         return true;
-
     }
 
 
