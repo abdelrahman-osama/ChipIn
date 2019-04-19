67e8fdff17c03662d6670bdc6e83818a96b9554f
abdelrahman <abdelrahman.o.a@ieee.org>
2019-04-19 11:49:32 +0200

    init

--- app/src/main/java/com/chipin/chipin/MapView.java
+++ app/src/main/java/com/chipin/chipin/MapView.java
@@ -3,12 +3,11 @@ package com.chipin.chipin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MapView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_map_view);
     }
 }
