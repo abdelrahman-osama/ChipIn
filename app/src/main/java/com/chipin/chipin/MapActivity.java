a45db0991b05f66493aa715da9b804d20cf60239
abdelrahman <abdelrahman.o.a@ieee.org>
2019-04-19 17:53:15 +0200

    fix:removed bottomsheet

--- app/src/main/java/com/chipin/chipin/MapActivity.java
+++ app/src/main/java/com/chipin/chipin/MapActivity.java
@@ -16,6 +16,7 @@ import android.view.ViewGroup;
 
 import com.andrefrsousa.superbottomsheet.SuperBottomSheetFragment;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.util.Objects;
@@ -23,25 +24,14 @@ import java.util.Objects;
 public class MapActivity extends AppCompatActivity {
 
 
-    BottomSheetFragment bottomSheetFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         MapView MapsFragment = new MapView();
         getSupportFragmentManager().beginTransaction().replace(R.id.map_container, MapsFragment, "").commit();
-        bottomSheetFragment = new BottomSheetFragment();
 
-
-        bottomSheetFragment.showNow(getSupportFragmentManager(), "BottomSheet");
-        bottomSheetFragment.setCancelable(false);
-
-//        MapsFragment.getView().setFocusable(true);
-//        bottomSheetFragment.setShowsDialog(true);
-
-//        bottomSheetFragment.onAttach(new View.OnAttachStateChangeListener());
 
     }
 
@@ -54,73 +44,4 @@ public class MapActivity extends AppCompatActivity {
     }
 
 
-
-
-
-}
-
-
-
-class BottomSheetFragment extends SuperBottomSheetFragment{
-
-//    BottomSheetBehavior mBottomSheetBehavior;
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        super.onCreateView(inflater, container, savedInstanceState);
-        View view  = inflater.inflate(R.layout.fragment_demo_sheet, container, false);
-//        mBottomSheetBehavior = BottomSheetBehavior.from(view.findViewById(R.id.bottom_sheet));
-//
-//
-//        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-//            @Override
-//            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-//                // React to state change
-//                Log.d("onStateChanged", "onStateChanged:" + newState);
-//                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
-////                    fab.setVisibility(View.GONE);
-//                } else {
-//                    if (newState == BottomSheetBehavior.STATE_COLLAPSED){
-//                        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
-//                    }
-////                    fab.setVisibility(View.VISIBLE);
-//                }
-//            }
-//
-//            @Override
-//            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-//                // React to dragging events
-//                Log.e("onSlide", "onSlide");
-//            }
-//        });
-//
-//        mBottomSheetBehavior.setPeekHeight(100);
-        return view;
-
-    }
-
-    @Override
-    public float getCornerRadius() {
-        return super.getCornerRadius();
-    }
-
-    @Override
-    public int getStatusBarColor() {
-        return super.getStatusBarColor();
-    }
-
-    @Override
-    public float getDim() {
-        return 0.1f;
-    }
-
-    @Override
-    public int getPeekHeight() {
-        return 100;
-    }
-
-    @Override
-    public boolean isCancelable() {
-        return false;
-    }
 }
